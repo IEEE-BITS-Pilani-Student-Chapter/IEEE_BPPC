1103058f938c611ecd43486e611e3e95e7d303cd
Akshay Mittal <mittalakshay2000@gmail.com>
2020-05-15 17:28:56 +0530

    blog and dev integrated

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from app.views import *
 
 urlpatterns = [
     path('', home, name="home"),
+    path('blogs/', blog_list, name='blogs'),
+    path('blogs/detail', blog_view, name='blog_view'),
     path('developers/', dev_page, name='dev_page'),
 ]
--- app/views.py
+++ app/views.py
@@ -12,3 +12,11 @@ def home(request):
 def dev_page(request):
     context = {}
     return render(request, 'app/dev.html', context);
+
+def blog_list(request):
+    context = {'posts': Post.objects.all()}
+    return render(request, 'app/blog_list.html', context)
+
+def blog_view(request):
+    context = {}
+    return render(request, 'app/blog_view.html', context)
