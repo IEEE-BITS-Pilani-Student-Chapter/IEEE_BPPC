48945588323702ac1a14c14315137b53fd5914db
ishaansarna23 <57299072+ishaansarna23@users.noreply.github.com>
2020-05-15 18:50:51 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,15 +3,18 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 
 from .models import Post
+from .forms import PostForm
 
 
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'app/home.html', context)
 
+
 def dev_page(request):
     context = {}
     return render(request, 'app/dev.html', context);
+
 
 def blog_list(request):
     context = {'posts': Post.objects.all()}
@@ -20,3 +23,17 @@ def blog_list(request):
 def blog_view(request):
     context = {}
     return render(request, 'app/blog_view.html', context)
+
+def post_image_view(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = PostForm()
+    return render(request, 'app/post_form.html', {'form': form})
+
+def success(request):
+    return HttpResponse('successfully uploaded')
