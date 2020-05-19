7f3029f6f1cf269dd511c29fdb2781fbfeb77ce5
ishaansarna23 <f20190388@pilani.bits-pilani.ac.in>
2020-05-19 14:57:13 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 class Post(models.Model):
     title = models.CharField(max_length=80)
     content = models.TextField()
@@ -12,14 +13,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
     def get_absolute_url(self):
         return reverse('blog_view', kwargs={'pk': self.pk})
 
-class Developers(models.Model):
-    name = models.CharField(max_length = 30)
-    post = models.CharField(max_length = 30)
+
+class Developer(models.Model):
+    name = models.CharField(max_length=30)
+    post = models.CharField(max_length=30)
     pic = models.ImageField(upload_to="developer/", editable=True, blank=True, default='person.png')
-    bio = models.CharField(max_length = 250)
+    bio = models.CharField(max_length=250)
     git = models.URLField(max_length=130, unique=True, blank=True)
     linkein = models.URLField(max_length=130, unique=True, blank=True)
 
