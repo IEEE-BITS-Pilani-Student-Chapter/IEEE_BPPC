6a6e1858f71e78f19603a1d7c0ccf7dcde979f0c
Mayank Jain <f20190141@pilani.bits-pilani.ac.in>
2020-06-27 17:03:31 +0530

    Toasters Added

--- static/js/script.js
+++ static/js/script.js
@@ -9,3 +9,26 @@
 //         });
 //     }
 // });
+
+$(document).ready(function() {
+    toastr.options = {
+        'closeButton': true,
+        'debug': false,
+        'newestOnTop': false,
+        'progressBar': false,
+        'positionClass': 'toast-bottom-right',
+        'preventDuplicates': false,
+        'showDuration': '1000',
+        'hideDuration': '1000',
+        'timeOut': '2000',
+        'extendedTimeOut': '1000',
+        'showEasing': 'swing',
+        'hideEasing': 'linear',
+        'showMethod': 'fadeIn',
+        'hideMethod': 'fadeOut',
+    }
+});
+
+$('.coming-soon').click(function(event) {
+    toastr.info('Coming Soon!')
+});
