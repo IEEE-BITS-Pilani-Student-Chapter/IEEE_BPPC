141773e343b34f93d05b78641cf2ecc12176b67a
Akshay Mittal <mittalakshay2000@gmail.com>
2020-06-28 18:32:53 +0530

    bug

--- IEEE_BPPC/settings.py
+++ IEEE_BPPC/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
